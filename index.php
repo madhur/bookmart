21142e617babe96adffd0e4eac825e5e7630bc81
Madhur <ahuja.madhur@gmail.com>
2014-09-07 21:07:28 +0530

    updated

---
+++ index.php
@@ -0,0 +1,79 @@
+   <?php include('header.inc.html'); ?>
+
+   <div class="row">
+            <div class="col-lg-12">
+                <img class="img-responsive" src="img/sale.png" width="100%" />
+            </div>
+
+        </div>
+
+       
+
+
+
+        <div class="row">
+            <div class="col-lg-12 ">
+                <h3>OUR BOOKS</h3>
+                <ul class="books">
+                    <li>
+                        <a href="#">
+                            <img src="img/book1.png" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="img/book2.png" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="img/book1.png" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="img/book2.png" />
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <img src="img/book1.png" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="img/book2.png" />
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <img src="img/book1.png" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="img/book2.png" />
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <img src="img/book1.png" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="img/book2.png" />
+                        </a>
+                    </li>
+
+
+
+                </ul>
+            </div>
+        </div>
+
+
+   <?php include('footer.inc.html'); ?>
---
+++ register.php
@@ -0,0 +1,77 @@
+   <?php 
+
+   include('header.inc.html'); 
+
+   if($_SERVER['REQUEST_METHOD']=='POST')
+   {
+        if(empty($_POST['username']) || empty($_POST['password']))
+        {
+            $error = 'Username and password are mandatory';
+        }
+
+        else
+        {
+            // validate from database
+            //
+
+
+        }
+
+   }
+
+   ?>
+
+<div class="row">
+ <div class="col-lg-10 darkBody" style="float: none; margin: 0 auto;">
+  <form class="form-horizontal" action='' method="POST">
+  <fieldset>
+    <div id="legend">
+      <legend class="">Register</legend>
+    </div>
+    <div class="control-group">
+      <!-- Username -->
+      <label class="control-label"  for="username">Username</label>
+      <div class="controls">
+        <input type="text" id="username" name="username" placeholder="" class="input-xlarge">
+        <p class="help-block">Username can contain any letters or numbers, without spaces</p>
+      </div>
+    </div>
+ 
+    <div class="control-group">
+      <!-- E-mail -->
+      <label class="control-label" for="email">E-mail</label>
+      <div class="controls">
+        <input type="text" id="email" name="email" placeholder="" class="input-xlarge">
+        <p class="help-block">Please provide your E-mail</p>
+      </div>
+    </div>
+ 
+    <div class="control-group">
+      <!-- Password-->
+      <label class="control-label" for="password">Password</label>
+      <div class="controls">
+        <input type="password" id="password" name="password" placeholder="" class="input-xlarge">
+        <p class="help-block">Password should be at least 4 characters</p>
+      </div>
+    </div>
+ 
+    <div class="control-group">
+      <!-- Password -->
+      <label class="control-label"  for="password_confirm">Password (Confirm)</label>
+      <div class="controls">
+        <input type="password" id="password_confirm" name="password_confirm" placeholder="" class="input-xlarge">
+        <p class="help-block">Please confirm password</p>
+      </div>
+    </div>
+ 
+    <div class="control-group">
+      <!-- Button -->
+      <div class="controls">
+        <button class="btn btn-success">Register</button>
+      </div>
+    </div>
+  </fieldset>
+</form>
+ </div>
+</div>
+   <?php include('footer.inc.html'); ?>
