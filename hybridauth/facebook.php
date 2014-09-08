af5c8db41c5af1eea2da08cbce6112ce48350555
Madhur <ahuja.madhur@gmail.com>
2014-09-09 00:24:20 +0530

    added hybrid auth

---
+++ hybridauth/facebook.php
@@ -0,0 +1,18 @@
+    <?php
+    // start a new session (required for Hybridauth)
+    session_start();
+    // change the following paths if necessary
+    $config = dirname(__FILE__) . '/config.php';
+    require_once( "Hybrid/Auth.php" );
+    try{
+    // create an instance for Hybridauth with the configuration file path as parameter
+    $hybridauth = new Hybrid_Auth( $config );
+    // try to authenticate the user with twitter,
+    // user will be redirected to Twitter for authentication,
+    // if he already did, then Hybridauth will ignore this step and return an instance of the adapter
+    $fb = $hybridauth->authenticate( "Facebook" );
+     
+   }
+    catch( Exception $e ){
+        echo "Error";
+    }
