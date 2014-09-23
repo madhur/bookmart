a37239d644d0a6dd509945d382b2259bff565949
Madhur <ahuja.madhur@gmail.com>
2014-09-24 00:03:17 +0530

    updated

--- hybridauth/facebook.php
+++ hybridauth/facebook.php
@@ -14,5 +14,25 @@
      
    }
     catch( Exception $e ){
-        echo "Error";
+        switch( $e->getCode() ){
+    case 0 : echo "Unspecified error."; break;
+    case 1 : echo "Hybriauth configuration error."; break;
+    case 2 : echo "Provider not properly configured."; break;
+    case 3 : echo "Unknown or disabled provider."; break;
+    case 4 : echo "Missing provider application credentials."; break;
+    case 5 : echo "Authentification failed. "
+    . "The user has canceled the authentication or the provider refused the connection.";
+    break;
+    case 6 : echo "User profile request failed. Most likely the user is not connected "
+    . "to the provider and he should authenticate again.";
+    $twitter->logout();
+    break;
+    case 7 : echo "User not connected to the provider.";
+    $twitter->logout();
+    break;
+    case 8 : echo "Provider does not support this feature."; break;
+    }
+     
+    // well, basically your should not display this to the end user, just give him a hint and move on..
+    echo "<br /><br /><b>Original error message:</b> " . $e->getMessage();
     }
